ed9ec84528607478a60344a152c5302849f64086
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2017-12-24 23:51:48 -0500

    clean up

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var cors = require('cors');
 var mockProperties = require('./data/property.json');
-// var path = require('path');
-// var fs = require('fs');
 
 const app = express();
 app.use(cors());
@@ -15,21 +13,6 @@ app.get('/', function (request, response) {
 
 app.get('/properties', function (req, res) {
   res.send(mockProperties);
-  // fs.readFile(mockProperties, 'utf8', function (err, data) {
-  //   if (err) {
-  //     // handle error
-  //     console.log(err);
-  //     return;
-  //   }
-  
-  //   jsonData = JSON.parse(data);
-  //   res.send(jsonData);
-  // });
-
-  // let readable = fs.createReadStream(propertyFilePath);
-  // readable.pipe(res);
-  // res.send('Hello World!' + propertyFilePath);
-
 });
 
 app.listen(app.get('port'), function () {
