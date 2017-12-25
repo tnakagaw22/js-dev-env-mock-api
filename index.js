a19da50c4e1ce1629eac01a68e7bb879cac8b368
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2017-12-24 23:50:04 -0500

    get a file by require and send it off to response

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-var path = require('path');
-var fs = require('fs');
+var mockProperties = require('./data/property.json');
+// var path = require('path');
+// var fs = require('fs');
 
 const app = express();
 app.use(cors());
@@ -13,10 +14,21 @@ app.get('/', function (request, response) {
 });
 
 app.get('/properties', function (req, res) {
-  let propertyFilePath = path.join(__dirname, 'data/property.json');
+  res.send(mockProperties);
+  // fs.readFile(mockProperties, 'utf8', function (err, data) {
+  //   if (err) {
+  //     // handle error
+  //     console.log(err);
+  //     return;
+  //   }
+  
+  //   jsonData = JSON.parse(data);
+  //   res.send(jsonData);
+  // });
+
   // let readable = fs.createReadStream(propertyFilePath);
   // readable.pipe(res);
-  res.send('Hello World!' + propertyFilePath);
+  // res.send('Hello World!' + propertyFilePath);
 
 });
 
