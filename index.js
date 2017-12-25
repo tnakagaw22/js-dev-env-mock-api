208c5a727211547e15d4e5241a3d85e4a24a0126
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2017-12-24 22:37:41 -0500

    removed es2015 from index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-import express from 'express';
-import cors from 'cors';
-import path from 'path';
-import fs from 'fs';
+var express = require('express');
+var cors = require('cors');
+var path = require('path');
+var fs = require('fs');
 
 const app = express();
 app.use(cors());
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.send('Hello World!')
 });
 
-app.get('/properties', function(req, res) {
+app.get('/properties', function (req, res) {
   let propertyFilePath = path.join(__dirname, 'data/property.json');
   let readable = fs.createReadStream(propertyFilePath);
   readable.pipe(res);
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port')); 
+app.listen(app.get('port'), function () {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
